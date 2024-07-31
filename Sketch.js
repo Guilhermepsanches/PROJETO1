15be38a50d06186edb99a981c9a39bac9d4639dd
Guilhermepsanches <00001108674641sp@al.educacao.sp.gov.br>
2024-07-31 14:53:32 -0300

    Create Sketch.js

---
+++ Sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(1000, 800);
+}
+
+function draw() {
+  background(220);
+}
+function setup() {
+  createCanvas(800, 5000);
+  background("rgb(241,102,232)");
+}
+function draw(){
+  fill("rgb(123,38,155)");
+  stroke ("lightblue");
+  if(mouseIsPressed)
+    circle (mouseX,mouseY,30,50);
+  
+}
+
+let cor;
+let vertical;
+let horizontal;
+
+function steup(){
+  creatiteCanvas(1000, 800);
+  background("white");
+  cor=color(random(0,255),random(0,255),random(0,255));
+  
+  
+}
